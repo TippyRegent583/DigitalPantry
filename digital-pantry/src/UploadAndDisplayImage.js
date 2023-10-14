133af78a628c6db3e9504ffb952ed45457c41e29
Anthony Candelmo <114012970+acandelmo3@users.noreply.github.com>
2023-10-14 18:32:26 -0400

    ingredient detection

--- digital-pantry/src/UploadAndDisplayImage.js
+++ digital-pantry/src/UploadAndDisplayImage.js
@@ -2,13 +2,36 @@ import React, { useState } from "react";
 
 const textFromImg = async (image) => {
   const { createWorker } = require('tesseract.js');
-  const worker = await createWorker('eng', 1, {
-    logger: m => console.log(m), // Add logger here
-  });
+  const worker = await createWorker('eng');
 
   (async () => {
     const { data: { text } } = await worker.recognize(image);
     const lines = text.split("\n");
+
+    const regEx1 = /\s(F|B)\s/g;
+    const foodLines = []
+    lines.forEach((line) => {
+      if (regEx1.test(line)) {
+        foodLines.push(line)
+      }
+    });
+
+    const regEx2 = /\b[a-zA-Z]{2,}\b/g;
+    const ingredients = []
+    foodLines.forEach((line) => {
+      const matches = line.match(regEx2)
+      let item = ""
+
+      if (matches != null) {
+        matches.forEach((word) => {
+          if (word != null) {
+            item += (word + ' ')
+          }
+        })
+        ingredients.push(item)
+      }
+    })
+    console.log("DONE!!!")
     await worker.terminate();
   })();
 }
